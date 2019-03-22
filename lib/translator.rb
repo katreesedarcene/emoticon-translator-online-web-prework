fdc8636a5dbcf15c444e200a4ae17610809cfb2d
Kat B <katreesebarnes@gmail.com>
2019-03-22 15:41:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,17 +26,10 @@ def get_japanese_emoticon(file_path, emoticon)
     "Sorry, that emoticon was not found"
   end
 end
-# def get_japanese_emoticon(file_path, emoticon)
-#   library = load_library(file_path)
-#   result = library["get_emoticon"][emoticon]
-#   if result
-#     result
-#   else
-#     "Sorry, that emoticon was not found"
-#   end
-# end
+
 
 
 def get_english_meaning
+  
   # code goes here
 end
