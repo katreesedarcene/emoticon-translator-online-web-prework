68ca4392f21699d8fbf9f9e0109c24a6e1b43952
Kat B <katreesebarnes@gmail.com>
2019-03-22 15:09:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ YAML.load_file(file).each do |english_name, japanese_emoticon|
 library  # code goes here
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
   # code goes here
 end
 
