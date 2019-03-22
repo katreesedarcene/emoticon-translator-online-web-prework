e1a89e0ca140d607312cd2cf628e82968cf8053e
Kat B <katreesebarnes@gmail.com>
2019-03-22 15:45:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -32,6 +32,10 @@ end
 def get_english_meaning(file_path, emoticon)
   library = load_library(file_path)
   english_meaning = library['get_meaning'][emoticon]
+  if english_meaning
+    english_meaning
+  else
+    
   
   # code goes here
 end
